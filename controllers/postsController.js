585205e000c617c836b718bc77c2ef8d7f2bd32a
Angel <angelo_stoyanov@abv.bg>
2022-07-10 22:52:51 +0300

    get allPosts page, index.js, service post getAll

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -8,9 +8,12 @@ const router = Router();
 
 router.get('/', async (req, res) => {
 
-
-
-    res.render('index')
+    const posts = await req.storage.getAll(req.query)
+    const ctx = {
+        title: 'Posts',
+        posts
+    }
+    res.render('index', ctx)
 })
 
 
@@ -26,31 +29,43 @@ router.post('/create', async (req, res) => {
 
     const post = {
 
-        title:req.body.title,
-        keyword:req.body.keyword,
-        location:req.body.location,
-        date:req.body.date,
-        imageUrl:req.body.imageUrl,
-        description:req.body.description,
+        title: req.body.title,
+        keyword: req.body.keyword,
+        location: req.body.location,
+        date: req.body.date,
+        imageUrl: req.body.imageUrl,
+        description: req.body.description,
 
     }
 
-    console.log(post,'--post--')
+    console.log(post, '--post--')
 
 
     try {
         await req.storage.create(post)
         res.redirect('/')
     } catch (err) {
-        let arr=err.message.split(', ')
-        const ctx={
-            title:'Create Post',
-            error:arr,
+        let arr = err.message.split(', ')
+        const ctx = {
+            title: 'Create Post',
+            error: arr,
             post
         }
-        res.render('create',ctx)
+        res.render('create', ctx)
     }
 })
+
+router.get('/allPosts', async (req, res) => {
+
+    const posts = await req.storage.getAll(req.query)
+    const ctx = {
+        title: 'Posts',
+        posts
+    }
+    res.render('allPosts', ctx)
+})
+
+
 
 
 module.exports = router;
--- services/posts.js
+++ services/posts.js
@@ -1,14 +1,24 @@
 const Post = require('../models/Post');
 
 
-async function create(post){
+async function create(post) {
     console.log(post)
-    const record=new Post(post)
+    const record = new Post(post)
     await record.save()
     return record;
 }
 
+async function getAll(query) {
+    const options = {}
+    if (query.search) {
+        option = { regex: query.search, $options: 'i' }
+    }
+    const posts = Post.find(options).lean()
+    return await posts
+}
 
-module.exports={
-    create
+
+module.exports = {
+    create,
+    getAll
 }
